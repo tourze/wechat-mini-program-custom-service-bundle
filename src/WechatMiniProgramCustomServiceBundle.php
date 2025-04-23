b4a4f2917002320caf01dea2d6de652f12df1d81
github-ci <you@example.com>
2025-04-23 08:22:43 +0000

    refactor(WechatMiniProgramCustomServiceBundle): 移除不必要的依赖和注解

--- src/WechatMiniProgramCustomServiceBundle.php
+++ src/WechatMiniProgramCustomServiceBundle.php
@@ -3,16 +3,7 @@
 namespace WechatMiniProgramCustomServiceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '小程序客服')]
-class WechatMiniProgramCustomServiceBundle extends Bundle implements BundleDependencyInterface
+class WechatMiniProgramCustomServiceBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \WechatMiniProgramBundle\WechatMiniProgramBundle::class => ['all' => true],
-        ];
-    }
 }
