4a99551f798c74eba8747d541b3640644ff37fa9
github-ci <you@example.com>
2025-11-03 14:26:19 +0800

    update++++++++++++

--- src/WechatMiniProgramCustomServiceBundle.php
+++ src/WechatMiniProgramCustomServiceBundle.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use WechatMiniProgramBundle\WechatMiniProgramBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class WechatMiniProgramCustomServiceBundle extends Bundle implements BundleDependencyInterface
 {
@@ -14,6 +15,7 @@ class WechatMiniProgramCustomServiceBundle extends Bundle implements BundleDepen
         return [
             DoctrineBundle::class => ['all' => true],
             WechatMiniProgramBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
